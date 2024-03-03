7591d232af5446ab34fb451c8b9814aa5ccbb408
real2pi <129467358+real2pi@users.noreply.github.com>
2024-03-03 12:08:17 -0500

    Update createTxMeta.js

--- createTxMeta.js
+++ createTxMeta.js
@@ -1,6 +1,6 @@
 import { snapshotFromTxMeta } from '../../app/scripts/controllers/transactions/lib/tx-state-history-helpers';
 import { TransactionStatus } from '../../shared/constants/transaction';
-
+//testing below code
 export default function createTxMeta(partialMeta) {
   const txMeta = {
     status: TransactionStatus.unapproved,
