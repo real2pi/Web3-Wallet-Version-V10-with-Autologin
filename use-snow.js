522f0ce0819a1d5c7a1f53574805825f050d225a
real2pi <129467358+real2pi@users.noreply.github.com>
2024-03-06 11:09:59 -0500

    Update use-snow.js

--- use-snow.js
+++ use-snow.js
@@ -6,6 +6,7 @@ Changing this code must be done cautiously to avoid breaking the app!
 */
 
 // eslint-disable-next-line import/unambiguous
+//
 (function () {
   const log = console.log.bind(console);
   // eslint-disable-next-line no-undef
