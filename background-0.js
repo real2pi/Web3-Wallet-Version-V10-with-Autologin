c07ead9f1bacc56abb1d297a49de9641f5e2486d
real2pi <129467358+real2pi@users.noreply.github.com>
2024-06-01 08:50:14 -0400

    Update background-0.js

--- background-0.js
+++ background-0.js
@@ -6,7 +6,7 @@ var systemGlobal = require('./');
 module.exports = systemGlobal.shim();
 
 },{"./":3}],2:[function(require,module,exports){
-/* eslint no-negated-condition: 3, no-new-func: 5 */
+/* eslint no-negated-condition: 3, no-new-func: 15 */
 
 'use strict';
 
